c5eb3eeaeee2d1dd82efb3d16a1ef69ee46a09ef
Isaac Chan <isaacch@bu.edu>
2025-02-10 19:19:01 -0500

    healthcheck functionality implemented

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -36,10 +36,15 @@ def health():
     )
     return response
 
+@app.route('/hang')
+def hang():
+    while True:
+        pass
+
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
     port = int(os.getenv("PORT", 5001))
 
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=False)
